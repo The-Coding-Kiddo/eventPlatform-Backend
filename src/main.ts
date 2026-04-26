f131514a90c12b05fbb5289030f1f0ec9d6640ac
mujtaba <mujtabaalhadi22@gmail.com>
2026-04-26 08:53:15 +0300

    global validation and Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // --- Swagger Setup ---
+  const config = new DocumentBuilder()
+    .setTitle('Event Platform API')
+    .setDescription('The core API for the Event Management Platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+  // ---------------------
 
   await app.listen(process.env.PORT ?? 3000);
 }
